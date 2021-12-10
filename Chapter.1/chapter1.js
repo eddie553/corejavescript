0006a75e59f0b639516f71f5a2202f12e76293f6
eddie553 <eddie553@naver.com>
2021-12-10 17:42:52 +0900

    1-17

--- Chapter.1/chapter1.js
+++ Chapter.1/chapter1.js
@@ -9,3 +9,19 @@ var copyObjectDeep = function (target) {
   }
   return result;
 };
+
+var obj = {
+  a: 1,
+  b: {
+    c: null,
+    d: [1, 2],
+  },
+};
+var obj2 = copyObjectDeep(obj);
+
+obj2.a = 3;
+obj2.b.c = 4;
+obj.b.d[1] = 3;
+
+console.log(obj); // { a: 1. b: { c: null, d: [1, 3] } }
+console.log(obj2); // { a: 3. b: { c: 4, d: { 0: 1, 1: 2 } } }
