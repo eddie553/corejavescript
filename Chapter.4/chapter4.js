88190dbceec2e62d9948965811fcdeda63aac700
eddie553 <eddie553@naver.com>
2021-12-10 20:10:43 +0900

    4-15

--- Chapter.4/chapter4.js
+++ Chapter.4/chapter4.js
@@ -1,34 +1,15 @@
-new Promise(function (resolve) {
-  setTimeout(function () {
-    var name = "에스프레소";
-    console.log(name);
-    resolve(name);
-  }, 500);
-})
-  .then(function (prevName) {
+var addCoffee = function (name) {
+  return function (prevName) {
     return new Promise(function (resolve) {
       setTimeout(function () {
-        var name = prevName + ", 아메리카노";
-        console.log(name);
-        resolve(name);
+        var newName = prevName ? prevName + ", " + name : name;
+        console.log(newName);
+        resolve(newName);
       }, 500);
     });
-  })
-  .then(function (prevName) {
-    return new Promise(function (resolve) {
-      setTimeout(function () {
-        var name = prevName + ", 카페모카";
-        console.log(name);
-        resolve(name);
-      }, 500);
-    });
-  })
-  .then(function (prevName) {
-    return new Promise(function (resolve) {
-      setTimeout(function () {
-        var name = prevName + ", 카페라떼";
-        console.log(name);
-        resolve(name);
-      }, 500);
-    });
-  });
+  };
+};
+addCoffee("에스프레소")()
+  .then(addCoffee("아메리카노"))
+  .then(addCoffee("카페모카"))
+  .then(addCoffee("카페라떼"));
