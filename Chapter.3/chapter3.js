7c1656fb49c959db5823eb6883f9cc0f28b3467a
eddie553 <eddie553@naver.com>
2021-12-10 18:53:00 +0900

    3-2

--- Chapter.3/chapter3.js
+++ Chapter.3/chapter3.js
@@ -1,3 +1,3 @@
-console.log(this); // { alert: f(), atob: f(), blur: f(), btoa: f(), ... }
-console.log(window); // { alert: f(), atob: f(), blur: f(), btoa: f(), ... }
-console.log(this === window); // true
+console.log(this); // { process: { title: 'node', version: 'v10.13.0',... } }
+console.log(global); // { process: { title: 'node', version: 'v10.13.0',... } }
+console.log(this === global); // true
