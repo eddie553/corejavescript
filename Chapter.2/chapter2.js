7af6a13a4074ddd330f0d032025758e5798dd141
eddie553 <eddie553@naver.com>
2021-12-10 18:40:07 +0900

    2-10

--- Chapter.2/chapter2.js
+++ Chapter.2/chapter2.js
@@ -1,12 +1,12 @@
+var sum = function sum(a, b) {
+  // 함수 선언문은 전체를 호이스팅합니다.
+  return a + b;
+};
+var multiply; // 변수는 선언부만 끌어올립니다.
 console.log(sum(1, 2));
 console.log(multiply(3, 4));
 
-function sum(a, b) {
-  // 함수 선언문 sum
-  return a + b;
-}
-
-var multiply = function (a, b) {
-  // 함수 표현식 multiply
+multiply = function (a, b) {
+  // 변수의 할당부는 원래 자리에 남겨둡니다.
   return a * b;
 };
