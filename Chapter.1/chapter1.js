0315871a2b4da2b8333da352c2d33c10036f73be
eddie553 <eddie553@naver.com>
2021-12-10 17:46:41 +0900

    1-21

--- Chapter.1/chapter1.js
+++ Chapter.1/chapter1.js
@@ -1,9 +1,31 @@
-var arr1 = [];
-arr1.length = 3;
-console.log(arr1); // [empty x 3]
+var arr1 = [undefined, 1];
+var arr2 = [];
+arr2[1] = 1;
 
-var arr2 = new Array(3);
-console.log(arr2); // [empty x 3]
+arr1.forEach(function (v, i) {
+  console.log(v, i);
+}); // undefined 0 / 1 1
+arr2.forEach(function (v, i) {
+  console.log(v, i);
+}); // 1 1
 
-var arr3 = [undefined, undefined, undefined];
-console.log(arr3); // [undefined, undefined, undefined]
+arr1.map(function (v, i) {
+  return v + i;
+}); // [NaN, 2]
+arr2.map(function (v, i) {
+  return v + i;
+}); // [empty, 2]
+
+arr1.filter(function (v) {
+  return !v;
+}); // [undefined]
+arr2.filter(function (v) {
+  return !v;
+}); // []
+
+arr1.reduce(function (p, c, i) {
+  return p + c + i;
+}, ""); // undefined011
+arr2.reduce(function (p, c, i) {
+  return p + c + i;
+}, ""); // 11
