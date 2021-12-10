d87887016e02f7e1d78fb818ba1066509b931cba
eddie553 <eddie553@naver.com>
2021-12-10 19:30:17 +0900

    3-25

--- Chapter.3/chapter3.js
+++ Chapter.3/chapter3.js
@@ -1,4 +1,11 @@
-const numbers = [10, 20, 3, 16, 45];
-const max = Math.max(...numbers);
-const min = Math.min(...numbers);
-console.log(max, min); // 45 3
+var func = function (a, b, c, d) {
+  console.log(this, a, b, c, d);
+};
+func(1, 2, 3, 4); // Window{ ... } 1 2 3 4
+
+var bindFunc1 = func.bind({ x: 1 });
+bindFunc1(5, 6, 7, 8); // { x: 1 } 5 6 7 8
+
+var bindFunc2 = func.bind({ x: 1 }, 4, 5);
+bindFunc2(6, 7); // { x: 1 } 4 5 6 7
+bindFunc2(8, 9); // { x: 1 } 4 5 8 9
