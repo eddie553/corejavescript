48e7631df527c51e207abe274777d38c4f5d51c7
eddie553 <eddie553@naver.com>
2021-12-10 20:09:29 +0900

    4-14

--- Chapter.4/chapter4.js
+++ Chapter.4/chapter4.js
@@ -1,23 +1,34 @@
-var coffeeList = "";
-
-var addEspresso = function (name) {
-  coffeeList = name;
-  console.log(coffeeList);
-  setTimeout(addAmericano, 500, "아메리카노");
-};
-var addAmericano = function (name) {
-  coffeeList += ", " + name;
-  console.log(coffeeList);
-  setTimeout(addMocha, 500, "카페모카");
-};
-var addMocha = function (name) {
-  coffeeList += ", " + name;
-  console.log(coffeeList);
-  setTimeout(addLatte, 500, "카페라떼");
-};
-var addLatte = function (name) {
-  coffeeList += ", " + name;
-  console.log(coffeeList);
-};
-
-setTimeout(addEspresso, 500, "에스프레소");
+new Promise(function (resolve) {
+  setTimeout(function () {
+    var name = "에스프레소";
+    console.log(name);
+    resolve(name);
+  }, 500);
+})
+  .then(function (prevName) {
+    return new Promise(function (resolve) {
+      setTimeout(function () {
+        var name = prevName + ", 아메리카노";
+        console.log(name);
+        resolve(name);
+      }, 500);
+    });
+  })
+  .then(function (prevName) {
+    return new Promise(function (resolve) {
+      setTimeout(function () {
+        var name = prevName + ", 카페모카";
+        console.log(name);
+        resolve(name);
+      }, 500);
+    });
+  })
+  .then(function (prevName) {
+    return new Promise(function (resolve) {
+      setTimeout(function () {
+        var name = prevName + ", 카페라떼";
+        console.log(name);
+        resolve(name);
+      }, 500);
+    });
+  });
