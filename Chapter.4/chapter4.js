c21b33e8e6803b3e1dc177347723f7c4164ddff6
eddie553 <eddie553@naver.com>
2021-12-10 20:04:35 +0900

    4-8

--- Chapter.4/chapter4.js
+++ Chapter.4/chapter4.js
@@ -1,10 +1,11 @@
-var obj = {
-  vals: [1, 2, 3],
-  logValues: function (v, i) {
-    console.log(this, v, i);
+var obj1 = {
+  name: "obj1",
+  func: function () {
+    var self = this;
+    return function () {
+      console.log(self.name);
+    };
   },
 };
-obj.logValues(1, 2); // { vals: [1, 2, 3], logValues: f } 1 2
-[4, 5, 6].forEach(obj.logValues); // Window { ... } 4 0
-// Window { ... } 5 1
-// Window { ... } 6 2
+var callback = obj1.func();
+setTimeout(callback, 1000);
