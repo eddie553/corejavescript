a328498feeccc04d7b90b7583d9781b3bc9c51a1
eddie553 <eddie553@naver.com>
2021-12-10 18:54:44 +0900

    3-5

--- Chapter.3/chapter3.js
+++ Chapter.3/chapter3.js
@@ -1,9 +1,15 @@
 var a = 1;
-window.b = 2;
+delete window.a; // false
 console.log(a, window.a, this.a); // 1 1 1
+
+var b = 2;
+delete b; // false
 console.log(b, window.b, this.b); // 2 2 2
 
-window.a = 3;
-b = 4;
-console.log(a, window.a, this.a); // 3 3 3
-console.log(b, window.b, this.b); // 4 4 4
+window.c = 3;
+delete window.c; // true
+console.log(c, window.c, this.c); // Uncaught ReferenceError: c is not defined
+
+window.d = 4;
+delete d; // true
+console.log(d, window.d, this.d); // Uncaught ReferenceError: d is not defined
