a330f987c99354d0f2b43c4af17087e31960e11d
eddie553 <eddie553@naver.com>
2021-12-10 18:58:23 +0900

    3-11

--- Chapter.3/chapter3.js
+++ Chapter.3/chapter3.js
@@ -1,16 +1,10 @@
 var obj = {
   outer: function () {
     console.log(this); // (1) { outer: f }
-    var innerFunc1 = function () {
-      console.log(this); // (2) Window { ... }
+    var innerFunc = () => {
+      console.log(this); // (2) { outer: f }
     };
-    innerFunc1();
-
-    var self = this;
-    var innerFunc2 = function () {
-      console.log(self); // (3) { outer: f }
-    };
-    innerFunc2();
+    innerFunc();
   },
 };
 obj.outer();
