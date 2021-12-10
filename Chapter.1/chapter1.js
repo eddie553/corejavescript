0ef24a7b58ded95ef3567963064727a0737aa846
eddie553 <eddie553@naver.com>
2021-12-10 17:41:40 +0900

    1-15

--- Chapter.1/chapter1.js
+++ Chapter.1/chapter1.js
@@ -14,13 +14,12 @@ var user = {
     facebook: "http://facebook.com/abc",
   },
 };
-var user2 = copyObject(user);
-user2.name = "Jung";
 
-console.log(user.name === user2.name); // false
+var user2 = copyObject(user);
+user2.urls = copyObject(user.urls);
 
 user.urls.portfolio = "http://portfolio.com";
-console.log(user.urls.portfolio === user2.urls.portfolio); // true
+console.log(user.urls.portfolio === user2.urls.portfolio); // false
 
 user2.urls.blog = "";
-console.log(user.urls.blog === user2.urls.blog); // true
+console.log(user.urls.blog === user2.urls.blog); // false
