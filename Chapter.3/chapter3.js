fce90e8d2dc947ef7b0d13db7d5fc110447531f6
eddie553 <eddie553@naver.com>
2021-12-10 19:19:25 +0900

    3-12

--- Chapter.3/chapter3.js
+++ Chapter.3/chapter3.js
@@ -1,10 +1,14 @@
-var obj = {
-  outer: function () {
-    console.log(this); // (1) { outer: f }
-    var innerFunc = () => {
-      console.log(this); // (2) { outer: f }
-    };
-    innerFunc();
-  },
-};
-obj.outer();
+setTimeout(function () {
+  console.log(this);
+}, 300); // (1)
+
+[1, 2, 3, 4, 5].forEach(function (x) {
+  // (2)
+  console.log(this, x);
+});
+
+document.body.innerHTML += '<button id="a">클릭</button>';
+document.body.querySelector("#a").addEventListener("click", function (e) {
+  // (3)
+  console.log(this, e);
+});
