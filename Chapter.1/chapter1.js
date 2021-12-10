356d37cdc740955d7851a1a907bc0195c10d8690
eddie553 <eddie553@naver.com>
2021-12-10 17:38:04 +0900

    1-10

--- Chapter.1/chapter1.js
+++ Chapter.1/chapter1.js
@@ -1,8 +1,18 @@
-var a = 10;
-var b = a;
-var obj1 = { c: 10, d: "ddd" };
-var obj2 = obj1;
+var user = {
+  name: "Jaenam",
+  gender: "male",
+};
 
-b = 15;
-obj2 = { c: 20, d: "ddd" };
-console.log(obj2);
+var changeName = function (user, newName) {
+  var newUser = user;
+  newUser.name = newName;
+  return newUser;
+};
+
+var user2 = changeName(user, "Jung");
+
+if (user !== user2) {
+  console.log("유저 정보가 변경되었습니다.");
+}
+console.log(user.name, user2.name); // Jung Jung
+console.log(user === user2); // true
