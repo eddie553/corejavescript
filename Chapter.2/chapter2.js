fcf780bbc6b13b790f5b5db1b2d7b92a737b16bc
eddie553 <eddie553@naver.com>
2021-12-10 18:13:41 +0900

    2-6

--- Chapter.2/chapter2.js
+++ Chapter.2/chapter2.js
@@ -1,8 +1,10 @@
 function a() {
+  var b; // 수집 대상 1. 변수는 선언부만 끌어올립니다.
+  function b() {} // 수집 대상 2. 함수 선언은 전체를 끌어올립니다.
+
   console.log(b); // (1)
-  var b = "bbb"; // 수집 대상 1(변수 선언)
+  b = "bbb"; // 변수의 할당부는 원래 자리에 남겨둡니다.
   console.log(b); // (2)
-  function b() {} // 수집 대상 2(함수 선언)
   console.log(b); // (3)
 }
 a();
