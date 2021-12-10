5705ef05d792abe95f0123c007cd563f4ce357d6
eddie553 <eddie553@naver.com>
2021-12-10 18:39:13 +0900

    2-9

--- Chapter.2/chapter2.js
+++ Chapter.2/chapter2.js
@@ -1,15 +1,12 @@
-function a() {
-  /* ... */
-} // 함수 선언문. 함수명 a가 곧 변수명.
-a(); // 실행 OK.
+console.log(sum(1, 2));
+console.log(multiply(3, 4));
 
-var b = function () {
-  /* ... */
-}; // (익명) 함수 표현식. 변수명 b가 곧 함수명.
-b(); // 실행 OK.
+function sum(a, b) {
+  // 함수 선언문 sum
+  return a + b;
+}
 
-var c = function d() {
-  /* ... */
-}; // 기명 함수 표현식. 변수명은 c, 함수명은 d.
-c(); // 실행 OK.
-d(); // 에러!
+var multiply = function (a, b) {
+  // 함수 표현식 multiply
+  return a * b;
+};
