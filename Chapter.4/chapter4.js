0919d8b67ac717a6fc42a27811fa7b7f33ac7aa2
eddie553 <eddie553@naver.com>
2021-12-10 20:08:32 +0900

    4-13

--- Chapter.4/chapter4.js
+++ Chapter.4/chapter4.js
@@ -1,35 +1,23 @@
-setTimeout(
-  function (name) {
-    var coffeeList = name;
-    console.log(coffeeList);
+var coffeeList = "";
 
-    setTimeout(
-      function (name) {
-        coffeeList += ", " + name;
-        console.log(coffeeList);
+var addEspresso = function (name) {
+  coffeeList = name;
+  console.log(coffeeList);
+  setTimeout(addAmericano, 500, "아메리카노");
+};
+var addAmericano = function (name) {
+  coffeeList += ", " + name;
+  console.log(coffeeList);
+  setTimeout(addMocha, 500, "카페모카");
+};
+var addMocha = function (name) {
+  coffeeList += ", " + name;
+  console.log(coffeeList);
+  setTimeout(addLatte, 500, "카페라떼");
+};
+var addLatte = function (name) {
+  coffeeList += ", " + name;
+  console.log(coffeeList);
+};
 
-        setTimeout(
-          function (name) {
-            coffeeList += ", " + name;
-            console.log(coffeeList);
-
-            setTimeout(
-              function (name) {
-                coffeeList += ", " + name;
-                console.log(coffeeList);
-              },
-              500,
-              "카페라떼"
-            );
-          },
-          500,
-          "카페모카"
-        );
-      },
-      500,
-      "아메리카노"
-    );
-  },
-  500,
-  "에스프레소"
-);
+setTimeout(addEspresso, 500, "에스프레소");
