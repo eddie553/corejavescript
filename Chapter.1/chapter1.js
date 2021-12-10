8356a217bea1dd12594d96b17b3e03f7cc8d73f7
eddie553 <eddie553@naver.com>
2021-12-10 17:42:11 +0900

    1-16

--- Chapter.1/chapter1.js
+++ Chapter.1/chapter1.js
@@ -1,25 +1,11 @@
-var copyObject = function (target) {
+var copyObjectDeep = function (target) {
   var result = {};
-  for (var prop in target) {
-    result[prop] = target[prop];
+  if (typeof target === "object" && target !== null) {
+    for (var prop in target) {
+      result[prop] = copyObjectDeep(target[prop]);
+    }
+  } else {
+    result = target;
   }
   return result;
 };
-
-var user = {
-  name: "Jaenam",
-  urls: {
-    portfolio: "http://github.com/abc",
-    blog: "http://blog.com",
-    facebook: "http://facebook.com/abc",
-  },
-};
-
-var user2 = copyObject(user);
-user2.urls = copyObject(user.urls);
-
-user.urls.portfolio = "http://portfolio.com";
-console.log(user.urls.portfolio === user2.urls.portfolio); // false
-
-user2.urls.blog = "";
-console.log(user.urls.blog === user2.urls.blog); // false
