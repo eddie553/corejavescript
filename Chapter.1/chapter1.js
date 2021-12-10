1df971f7e2a2f8e5ecdfa53159880bca381214d3
eddie553 <eddie553@naver.com>
2021-12-10 17:40:32 +0900

    1-13

--- Chapter.1/chapter1.js
+++ Chapter.1/chapter1.js
@@ -5,3 +5,17 @@ var copyObject = function (target) {
   }
   return result;
 };
+
+var user = {
+  name: "Jaenam",
+  gender: "male",
+};
+
+var user2 = copyObject(user);
+user2.name = "Jung";
+
+if (user !== user2) {
+  console.log("유저 정보가 변경되었습니다."); // 유저 정보가 변경되었습니다.
+}
+console.log(user.name, user2.name); // Jaenam Jung
+console.log(user === user2); // false
