10ff471ed33fe589aea1e91bff2b0fcd29ccb4e9
eddie553 <eddie553@naver.com>
2021-12-10 18:13:03 +0900

    2-5

--- Chapter.2/chapter2.js
+++ Chapter.2/chapter2.js
@@ -1,12 +1,8 @@
 function a() {
-  var x; // 수집 대상 1의 변수 선언 부분
-  var x; // 수집 대상 2의 변수 선언 부분
-  var x; // 수집 대상 3의 변수 선언 부분
-
-  x = 1; // 수집 대상 1의 할당 부분
-  console.log(x); // (1)
-  console.log(x); // (2)
-  x = 2; // 수집 대상 3의 할당 부분
-  console.log(x); // (3)
+  console.log(b); // (1)
+  var b = "bbb"; // 수집 대상 1(변수 선언)
+  console.log(b); // (2)
+  function b() {} // 수집 대상 2(함수 선언)
+  console.log(b); // (3)
 }
-a(1);
+a();
