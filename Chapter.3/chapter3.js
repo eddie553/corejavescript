afd6e6b366f038079e6281c2a5252da4a0b75c21
eddie553 <eddie553@naver.com>
2021-12-10 19:21:48 +0900

    3-16

--- Chapter.3/chapter3.js
+++ Chapter.3/chapter3.js
@@ -1,9 +1,12 @@
+var func = function (a, b, c) {
+  console.log(this, a, b, c);
+};
+func.apply({ x: 1 }, [4, 5, 6]); // { x: 1 } 4 5 6
+
 var obj = {
   a: 1,
   method: function (x, y) {
     console.log(this.a, x, y);
   },
 };
-
-obj.method(2, 3); // 1 2 3
-obj.method.call({ a: 4 }, 5, 6); // 4 5 6
+obj.method.apply({ a: 4 }, [5, 6]); // 4 5 6
