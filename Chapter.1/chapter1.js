b3f175ccdd076d526abb950cd021f6d5697320ff
eddie553 <eddie553@naver.com>
2021-12-10 17:43:55 +0900

    1-18

--- Chapter.1/chapter1.js
+++ Chapter.1/chapter1.js
@@ -1,27 +1,24 @@
-var copyObjectDeep = function (target) {
-  var result = {};
-  if (typeof target === "object" && target !== null) {
-    for (var prop in target) {
-      result[prop] = copyObjectDeep(target[prop]);
-    }
-  } else {
-    result = target;
-  }
-  return result;
+var copyObjectViaJSON = function (target) {
+  return JSON.parse(JSON.stringify(target));
 };
-
 var obj = {
   a: 1,
   b: {
     c: null,
     d: [1, 2],
+    func1: function () {
+      console.log(3);
+    },
+  },
+  func2: function () {
+    console.log(4);
   },
 };
-var obj2 = copyObjectDeep(obj);
+var obj2 = copyObjectViaJSON(obj);
 
 obj2.a = 3;
 obj2.b.c = 4;
 obj.b.d[1] = 3;
 
-console.log(obj); // { a: 1. b: { c: null, d: [1, 3] } }
-console.log(obj2); // { a: 3. b: { c: 4, d: { 0: 1, 1: 2 } } }
+console.log(obj); // { a: 1. b: { c: null, d: [1, 3], func1: f() }, func2: f() }
+console.log(obj2); // { a: 3. b: { c: 4,    d: [1, 2] } }
