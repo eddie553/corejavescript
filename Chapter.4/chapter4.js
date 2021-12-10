c6bb8998e26fc938e1aec76fffdb85476b98f6e5
eddie553 <eddie553@naver.com>
2021-12-10 20:05:37 +0900

    4-10

--- Chapter.4/chapter4.js
+++ Chapter.4/chapter4.js
@@ -4,4 +4,13 @@ var obj1 = {
     console.log(obj1.name);
   },
 };
-setTimeout(obj1.func, 1000);
+var obj2 = {
+  name: "obj2",
+  func: obj1.func,
+};
+var callback2 = obj2.func();
+setTimeout(callback2, 1500);
+
+var obj3 = { name: "obj3" };
+var callback3 = obj1.func.call(obj3);
+setTimeout(callback3, 2000);
