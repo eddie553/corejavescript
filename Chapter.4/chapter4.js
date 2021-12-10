9397adf555b39050d39ca5700867887c01104978
eddie553 <eddie553@naver.com>
2021-12-10 20:01:48 +0900

    4-5

--- Chapter.4/chapter4.js
+++ Chapter.4/chapter4.js
@@ -1,11 +1,8 @@
-var newArr2 = [10, 20, 30].map(function (index, currentValue) {
-  console.log(index, currentValue);
-  return currentValue + 5;
-});
-console.log(newArr2);
-
-// -- 실행 결과 --
-// 10 0
-// 20 1
-// 30 2
-// [5, 6, 7]
+Array.prototype.map = function (callback, thisArg) {
+  var mappedArr = [];
+  for (var i = 0; i < this.length; i++) {
+    var mappedValue = callback.call(thisArg || window, this[i], i, this);
+    mappedArr[i] = mappedValue;
+  }
+  return mappedArr;
+};
