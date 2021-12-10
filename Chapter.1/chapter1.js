04a85ff8f3c847da7798eec9c153e9a4fc39fd50
eddie553 <eddie553@naver.com>
2021-12-10 17:45:30 +0900

    1-20

--- Chapter.1/chapter1.js
+++ Chapter.1/chapter1.js
@@ -1,11 +1,9 @@
-var a;
-console.log(a); // (1) undefined. 값을 대입하지 않은 변수에 접근
+var arr1 = [];
+arr1.length = 3;
+console.log(arr1); // [empty x 3]
 
-var obj = { a: 1 };
-console.log(obj.a); // 1
-console.log(obj.b); // (2) 존재하지 않는 프로퍼티에 접근
-console.log(b); // c.f) ReferenceError: b is not defined
+var arr2 = new Array(3);
+console.log(arr2); // [empty x 3]
 
-var func = function () {};
-var c = func(); // (3) 반환(return)값이 없으면 undefined를 반환한 것으로 간주.
-console.log(c); // undefined
+var arr3 = [undefined, undefined, undefined];
+console.log(arr3); // [undefined, undefined, undefined]
