c308154e16360619b88677a34787eb0df78e8dc4
eddie553 <eddie553@naver.com>
2021-12-10 18:11:52 +0900

    2-3

--- Chapter.2/chapter2.js
+++ Chapter.2/chapter2.js
@@ -1,9 +1,9 @@
-function a(x) {
-  // 수집 대상 1(매개변수)
+function a() {
+  var x = 1; // 수집 대상 1(매개변수 선언)
   console.log(x); // (1)
   var x; // 수집 대상 2(변수 선언)
   console.log(x); // (2)
   var x = 2; // 수집 대상 3(변수 선언)
   console.log(x); // (3)
 }
-a(1);
+a();
