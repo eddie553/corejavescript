e85af63bbdaa20a647c4ca14f1cf6e0e14036443
eddie553 <eddie553@naver.com>
2021-12-10 13:20:49 +0900

    1-3

--- Chapter.1/chapter1.js
+++ Chapter.1/chapter1.js
@@ -1,5 +1,7 @@
-var a; // 변수 a 선언
-a = "abc"; // 변수 a에 데이터 할당
+var a = "abc";
+a = a + "def";
 
-var a = "abc"; // 변수 선언과 할당을 한 문장으로 표현
-console.log(a);
+var b = 5;
+var c = 5;
+b = 7;
+console.log(a, b);
