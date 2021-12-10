4ca342938983642ecd71341e9830373249ec6819
eddie553 <eddie553@naver.com>
2021-12-10 18:54:00 +0900

    3-4

--- Chapter.3/chapter3.js
+++ Chapter.3/chapter3.js
@@ -1,4 +1,9 @@
 var a = 1;
-console.log(a); // 1
-console.log(window.a); // 1
-console.log(this.a); // 1
+window.b = 2;
+console.log(a, window.a, this.a); // 1 1 1
+console.log(b, window.b, this.b); // 2 2 2
+
+window.a = 3;
+b = 4;
+console.log(a, window.a, this.a); // 3 3 3
+console.log(b, window.b, this.b); // 4 4 4
