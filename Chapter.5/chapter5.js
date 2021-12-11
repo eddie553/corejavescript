9f2588f72926c23204a3c88cbc7df3fb3aab21ec
eddie553 <eddie553@naver.com>
2021-12-11 17:41:02 +0900

    5-13

--- Chapter.5/chapter5.js
+++ Chapter.5/chapter5.js
@@ -1,41 +1,9 @@
-var createCar = function () {
-  var fuel = Math.ceil(Math.random() * 10 + 10); // 연료(L)
-  var power = Math.ceil(Math.random() * 3 + 2); // 연비(km / L)
-  var moved = 0; // 총 이동거리
-  var publicMembers = {
-    get moved() {
-      return moved;
-    },
-    run: function () {
-      var km = Math.ceil(Math.random() * 6);
-      var wasteFuel = km / power;
-      if (fuel < wasteFuel) {
-        console.log("이동불가");
-        return;
-      }
-      fuel -= wasteFuel;
-      moved += km;
-      console.log(km + "km 이동 (총 " + moved + "km). 남은 연료: " + fuel);
-    },
-  };
-  Object.freeze(publicMembers);
-  return publicMembers;
+var add = function () {
+  var result = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    result += arguments[i];
+  }
+  return result;
 };
-var car = createCar();
-
-car.run(); // random 거리 계산
-console.log(car.moved); // random
-console.log(car.fuel); //undefined
-console.log(car.power); //undefined
-
-car.fuel = 1000;
-console.log(car.fuel); //1000
-car.run();
-
-car.power = 100;
-console.log(car.power); //100
-car.run();
-
-car.moved = 1000;
-console.log(car.moved); // random
-car.run();
+var addPartial = add.bind(null, 1, 2, 3, 4, 5);
+console.log(addPartial(6, 7, 8, 9, 10)); // 55
