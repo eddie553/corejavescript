c991832639b953566e6cd70056f394046cf090c2
eddie553 <eddie553@naver.com>
2021-12-11 17:47:17 +0900

    5-18

--- Chapter.5/chapter5.js
+++ Chapter.5/chapter5.js
@@ -1,15 +1,15 @@
-var curry3 = function (func) {
+var curry5 = function (func) {
   return function (a) {
     return function (b) {
-      return func(a, b);
+      return function (c) {
+        return function (d) {
+          return function (e) {
+            return func(a, b, c, d, e);
+          };
+        };
+      };
     };
   };
 };
-
-var getMaxWith10 = curry3(Math.max)(10);
-console.log(getMaxWith10(8)); // 10
-console.log(getMaxWith10(25)); // 25
-
-var getMinWith10 = curry3(Math.min)(10);
-console.log(getMinWith10(8)); // 8
-console.log(getMinWith10(25)); // 10
+var getMax = curry5(Math.max);
+console.log(getMax(1)(2)(3)(4)(5)); // 5
