e8b2eb5865ee009cf133e1583c4e0831e254f264
eddie553 <eddie553@naver.com>
2021-12-11 17:27:41 +0900

    5-4-2

--- Chapter.5/chapter5.js
+++ Chapter.5/chapter5.js
@@ -1,12 +1,10 @@
-// (1) setInterval/setTimeout
+// (2) eventListener
 (function () {
-  var a = 0;
-  var intervalId = null;
-  var inner = function () {
-    if (++a >= 10) {
-      clearInterval(intervalId);
-    }
-    console.log(a);
-  };
-  intervalId = setInterval(inner, 1000);
+  var count = 0;
+  var button = document.createElement("button");
+  button.innerText = "click";
+  button.addEventListener("click", function () {
+    console.log(++count, "times clicked");
+  });
+  document.body.appendChild(button);
 })();
