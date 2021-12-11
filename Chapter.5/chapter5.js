c923149984b99e51da50db2f18dfc150312f4548
eddie553 <eddie553@naver.com>
2021-12-11 17:33:27 +0900

    5-9

--- Chapter.5/chapter5.js
+++ Chapter.5/chapter5.js
@@ -1,13 +1,15 @@
 var fruits = ["apple", "banana", "peach"];
 var $ul = document.createElement("ul");
 
-var alertFruit = function (fruit) {
-  alert("your choice is " + fruit);
+var alertFruitBuilder = function (fruit) {
+  return function () {
+    alert("your choice is " + fruit);
+  };
 };
 fruits.forEach(function (fruit) {
   var $li = document.createElement("li");
   $li.innerText = fruit;
-  $li.addEventListener("click", alertFruit.bind(null, fruit));
+  $li.addEventListener("click", alertFruitBuilder(fruit));
   $ul.appendChild($li);
 });
 document.body.appendChild($ul);
